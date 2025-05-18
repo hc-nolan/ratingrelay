81e18a312df38d2fc74a67c02ba20d26165eefc0
hnolan <hayden@hnolan.ca>
2025-05-17 21:01:52 -0400

    Add test for to_Track_list

---
+++ src/tests/test_ratingrelay.py
@@ -0,0 +1,22 @@
+import pytest
+from ratingrelay import to_Track_list
+from util import Track
+from plexapi.audio import Track as PlexTrack
+
+
+def mock_track(mocker, title: str, artist: str):
+    track = mocker.MagicMock()
+    track.title = title
+    artist_mock = mocker.MagicMock()
+    artist_mock.title = artist
+    track.artist.return_value = artist_mock
+    return track
+
+
+class TestToTrackList:
+    """Tests for to_Track_list"""
+
+    def test_success(self, mocker):
+        plex_track_list = [mock_track(mocker, "1", "2"), mock_track(mocker, "3", "4")]
+        expected = [Track(title="1", artist="2"), Track(title="3", artist="4")]
+        assert to_Track_list(plex_track_list) == expected

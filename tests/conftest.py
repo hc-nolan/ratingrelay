9680bd72cd1cc05b50a6cfb2aec3a58c1b09c5a0
hnolan <hayden@hnolan.ca>
2025-11-30 18:03:45 -0500

    Add reset fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,15 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from ratingrelay import setup_services
 from ratingrelay.config import settings
+from ratingrelay.reset import reset
 
 
 @pytest.fixture
 def services():
     return setup_services(settings)
+
+
+@pytest.fixture
+def cleanup(services):
+    """When tests are done, reset all services"""
+    reset(services)

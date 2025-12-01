a95d97573e67b1707d2436b96c86917d18d8bdd6
hnolan <hayden@hnolan.ca>
2025-11-30 23:12:52 -0500

    Add test for unloving/unhating Plex->LBZ

--- tests/test_plex_to_lbz.py
+++ tests/test_plex_to_lbz.py
@@ -43,3 +43,69 @@ def test_plex_relay_hates_to_listenbrainz(services, cleanup):
 
     lbz_hates = services.lbz.all_hates()
     assert len(lbz_hates) == 10
+
+
+def test_plex_unlove(services, cleanup):
+    """
+    Test that when we un-love a track on Plex, it is un-loved on ListenBrainz
+    next sync
+    """
+    plex = services.plex
+
+    # Make sure we're starting with no loved tracks
+    loves = plex.get_loved_tracks()
+    assert len(loves) == 0
+
+    # Search for an arbitrary track, then love it
+    track_search = plex.music_library.search(libtype="track", limit=1)
+    plex_track = track_search[0]
+    plex.submit_rating(plex_track, plex.love_threshold)
+
+    services.lfm = None
+    plex_relay_loves(services)
+
+    lbz_loves = services.lbz.all_loves()
+    assert len(lbz_loves) == 1
+
+    # Un-love the track
+    plex.submit_rating(plex_track, plex.love_threshold - 1)
+
+    # Sync again
+    plex_relay_loves(services)
+
+    # Check that the track was unloved from ListenBrainz
+    lbz_loves_after = services.lbz.all_loves()
+    assert len(lbz_loves_after) == 0
+
+
+def test_plex_unhate(services, cleanup):
+    """
+    Test that when we un-hate a track on Plex, it is un-hated on ListenBrainz
+    next sync
+    """
+    plex = services.plex
+
+    # Make sure we're starting with no hated tracks
+    hates = plex.get_hated_tracks()
+    assert len(hates) == 0
+
+    # Search for an arbitrary track, then hate it
+    track_search = plex.music_library.search(libtype="track", limit=1)
+    plex_track = track_search[0]
+    plex.submit_rating(plex_track, plex.hate_threshold)
+
+    services.lfm = None
+    plex_relay_hates(services)
+
+    lbz_hates = services.lbz.all_hates()
+    assert len(lbz_hates) == 1
+
+    # Un-hate the track
+    plex.submit_rating(plex_track, plex.hate_threshold + 1)
+
+    # Sync again
+    plex_relay_hates(services)
+
+    # Check that the track was unhated from ListenBrainz
+    lbz_hates_after = services.lbz.all_hates()
+    assert len(lbz_hates_after) == 0

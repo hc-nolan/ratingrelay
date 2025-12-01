5787169eb6e5f378fb04d52d345a9112ab8d9a8e
hnolan <hayden@hnolan.ca>
2025-11-30 21:18:34 -0500

    Test multiple track relay

--- tests/test_plex_sync.py
+++ tests/test_plex_sync.py
@@ -1,5 +1,5 @@
 import pytest
-from ratingrelay.relay import plex_relay
+from ratingrelay.relay import plex_relay_loves
 
 
 def test_plex_love_track(services, cleanup):
@@ -15,20 +15,37 @@ def test_plex_love_track(services, cleanup):
 
     # Search for an arbitrary track, then love it
     track_search = plex.music_library.search(libtype="track", limit=1)
-    assert len(track_search) != 0
     track = track_search[0]
     plex.submit_rating(track, plex.love_threshold)
 
-    # Make sure it's returned by get_loved_tracks()
-    loves = plex.get_loved_tracks()
-    assert len(loves) == 1
-    assert loves[0].title == track.title
-
-    # Relay
-    plex_relay(services)
+    plex_relay_loves(services)
 
     lfm_loves = services.lfm.all_loves()
     lbz_loves = services.lbz.all_loves()
 
     assert lfm_loves[0].title == track.title
     assert lbz_loves[0].title == track.title
+
+
+def test_plex_love_multiple_tracks(services, cleanup):
+    """
+    Test that all tracks loved on Plex get synced to the services
+    """
+    plex = services.plex
+
+    # Make sure we're starting with no loved tracks
+    loves = plex.get_loved_tracks()
+    assert len(loves) == 0
+
+    # Search for 10 arbitrary tracks, then love them
+    track_search = plex.music_library.search(libtype="track", limit=10)
+    for track in track_search:
+        plex.submit_rating(track, plex.love_threshold)
+
+    plex_relay_loves(services)
+
+    lfm_loves = services.lfm.all_loves()
+    lbz_loves = services.lbz.all_loves()
+
+    assert len(lfm_loves) == 10
+    assert len(lbz_loves) == 10

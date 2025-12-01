2a6a71af1b91ea93fb32214522428d5310de7730
hnolan <hayden@hnolan.ca>
2025-11-30 22:57:15 -0500

    Add test for plex -> lbz hate relay

--- tests/test_plex_to_lbz.py
+++ tests/test_plex_to_lbz.py
@@ -1,4 +1,4 @@
-from ratingrelay.relay import plex_relay_loves
+from ratingrelay.relay import plex_relay_loves, plex_relay_hates
 
 
 def test_plex_relay_loves_to_listenbrainz(services, cleanup):
@@ -21,3 +21,25 @@ def test_plex_relay_loves_to_listenbrainz(services, cleanup):
 
     lbz_loves = services.lbz.all_loves()
     assert len(lbz_loves) == 10
+
+
+def test_plex_relay_hates_to_listenbrainz(services, cleanup):
+    """
+    Test that all tracks hated on Plex get synced to ListenBrainz
+    """
+    plex = services.plex
+
+    # Make sure we're starting with no hated tracks
+    hates = plex.get_hated_tracks()
+    assert len(hates) == 0
+
+    # Search for 10 arbitrary tracks, then hate them
+    track_search = plex.music_library.search(libtype="track", limit=10)
+    for track in track_search:
+        plex.submit_rating(track, plex.hate_threshold)
+
+    services.lfm = None
+    plex_relay_hates(services)
+
+    lbz_hates = services.lbz.all_hates()
+    assert len(lbz_hates) == 10

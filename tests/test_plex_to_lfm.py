2d6845b9910b7cdca37f199a0c33802178e6fd6e
hnolan <hayden@hnolan.ca>
2025-11-30 23:17:39 -0500

    Add test for unloving Plex->LFM

--- tests/test_plex_to_lfm.py
+++ tests/test_plex_to_lfm.py
@@ -21,3 +21,36 @@ def test_plex_relay_loves_to_lastfm(services, cleanup):
 
     lfm_loves = services.lfm.all_loves()
     assert len(lfm_loves) == 10
+
+
+def test_plex_unlove(services, cleanup):
+    """
+    Test that when we un-love a track on Plex, it is un-loved on LastFM
+    next sync
+    """
+    plex = services.plex
+
+    # Make sure we're starting with no loved tracks
+    loves = plex.get_loved_tracks()
+    assert len(loves) == 0
+
+    # Search for an arbitrary track, then love it
+    track_search = plex.music_library.search(libtype="track", limit=1)
+    plex_track = track_search[0]
+    plex.submit_rating(plex_track, plex.love_threshold)
+
+    services.lbz = None
+    plex_relay_loves(services)
+
+    lfm_loves = services.lfm.all_loves()
+    assert len(lfm_loves) == 1
+
+    # Un-love the track
+    plex.submit_rating(plex_track, plex.love_threshold - 1)
+
+    # Sync again
+    plex_relay_loves(services)
+
+    # Check that the track was unloved from LastFM
+    lfm_loves_after = services.lfm.all_loves()
+    assert len(lfm_loves_after) == 0

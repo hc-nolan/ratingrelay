ec7ca0ac2c81aed5d2335f31672c18961597c40c
hnolan <hayden@hnolan.ca>
2025-12-03 21:52:56 -0500

    Better handling for various data types for tracks in ListenBrainz.reset()

--- ratingrelay/listenbrainz.py
+++ ratingrelay/listenbrainz.py
@@ -92,10 +92,16 @@ class ListenBrainz:
         Reset a track's ListenBrainz rating to 0.
         """
         log.info(f"ListenBrainz - resetting track: {track}")
-        if isinstance(track, dict):
-            recording_mbid = track.get("rec_mbid")
-        elif isinstance(track, Track):
-            recording_mbid = track.mbid
+        match track:
+            case dict():
+                recording_mbid = track.get("rec_mbid")
+            case Track():
+                recording_mbid = track.mbid
+            case _:
+                raise ValueError(
+                    f"Invalid type for track argument: {track} - {type(track)}. "
+                    f"Supported types: Track, dict"
+                )
         self.client.submit_user_feedback(0, recording_mbid)
 
     def love(self, track: Track):

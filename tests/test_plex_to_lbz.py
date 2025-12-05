2a8d5274064f0c242d867a3b2d664bcfd648b5e7
hnolan <hayden@hnolan.ca>
2025-12-04 19:39:22 -0500

    Update test_plex_to_lbz with assert_relay_success()

--- tests/test_plex_to_lbz.py
+++ tests/test_plex_to_lbz.py
@@ -1,5 +1,6 @@
 from ratingrelay.config import settings
 from ratingrelay.relay import plex_relay_loves, plex_relay_hates
+from tests.conftest import assert_relay_success
 
 
 def test_plex_relay_loves_to_listenbrainz(services, cleanup):
@@ -7,21 +8,22 @@ def test_plex_relay_loves_to_listenbrainz(services, cleanup):
     Test that all tracks loved on Plex get synced to ListenBrainz
     """
     plex = services.plex
-
     # Make sure we're starting with no loved tracks
     loves = plex.get_loved_tracks()
     assert len(loves) == 0
-
     # Search for arbitrary tracks, then love them
     track_search = plex.music_library.search(libtype="track", limit=settings.test_limit)
     for track in track_search:
         plex.submit_rating(track, plex.love_threshold)
-
     services.lfm = None
     plex_relay_loves(services)
 
-    lbz_loves = services.lbz.all_loves()
-    assert len(lbz_loves) == settings.test_limit
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_loves(),
+        source_getter=plex.get_loved_tracks,
+        rating="love",
+    )
 
 
 def test_plex_relay_hates_to_listenbrainz(services, cleanup):
@@ -29,21 +31,22 @@ def test_plex_relay_hates_to_listenbrainz(services, cleanup):
     Test that all tracks hated on Plex get synced to ListenBrainz
     """
     plex = services.plex
-
     # Make sure we're starting with no hated tracks
     hates = plex.get_hated_tracks()
     assert len(hates) == 0
-
     # Search for arbitrary tracks, then hate them
     track_search = plex.music_library.search(libtype="track", limit=settings.test_limit)
     for track in track_search:
         plex.submit_rating(track, plex.hate_threshold)
-
     services.lfm = None
     plex_relay_hates(services)
 
-    lbz_hates = services.lbz.all_hates()
-    assert len(lbz_hates) == settings.test_limit
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_hates(),
+        source_getter=plex.get_hated_tracks,
+        rating="hate",
+    )
 
 
 def test_plex_unlove(services, cleanup):
@@ -52,29 +55,28 @@ def test_plex_unlove(services, cleanup):
     next sync
     """
     plex = services.plex
-
     # Make sure we're starting with no loved tracks
     loves = plex.get_loved_tracks()
     assert len(loves) == 0
-
     # Search for arbitrary tracks and then love them
     track_search = plex.music_library.search(libtype="track", limit=settings.test_limit)
     for plex_track in track_search:
         plex.submit_rating(plex_track, plex.love_threshold)
-
     services.lfm = None
     plex_relay_loves(services)
 
-    lbz_loves = services.lbz.all_loves()
-    assert len(lbz_loves) == settings.test_limit
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_loves(),
+        source_getter=plex.get_loved_tracks,
+        rating="love",
+    )
 
     # Un-love the tracks
     for plex_track in track_search:
         plex.submit_rating(plex_track, plex.love_threshold - 1)
-
     # Sync again
     plex_relay_loves(services)
-
     # Check that the track was unloved from ListenBrainz
     lbz_loves_after = services.lbz.all_loves()
     assert len(lbz_loves_after) == 0
@@ -86,29 +88,28 @@ def test_plex_unhate(services, cleanup):
     next sync
     """
     plex = services.plex
-
     # Make sure we're starting with no hated tracks
     hates = plex.get_hated_tracks()
     assert len(hates) == 0
-
     # Search for arbitrary tracks and then hate them
     track_search = plex.music_library.search(libtype="track", limit=settings.test_limit)
     for plex_track in track_search:
         plex.submit_rating(plex_track, plex.hate_threshold)
-
     services.lfm = None
     plex_relay_hates(services)
 
-    lbz_hates = services.lbz.all_hates()
-    assert len(lbz_hates) == settings.test_limit
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_hates(),
+        source_getter=plex.get_hated_tracks,
+        rating="hate",
+    )
 
     # Un-hate the tracks
     for plex_track in track_search:
         plex.submit_rating(plex_track, plex.hate_threshold + 1)
-
     # Sync again
     plex_relay_hates(services)
-
     # Check that the tracks were unhated from ListenBrainz
     lbz_hates_after = services.lbz.all_hates()
     assert len(lbz_hates_after) == 0
@@ -120,32 +121,37 @@ def test_lbz_unlove(services, cleanup):
     on ListenBrainz, it should be re-loved next sync
     """
     plex = services.plex
-
     # Make sure we're starting with no loved tracks
     assert len(plex.get_loved_tracks()) == 0
-
     # Search for arbitrary tracks and then love them
     track_search = plex.music_library.search(libtype="track", limit=settings.test_limit)
     for plex_track in track_search:
         plex.submit_rating(plex_track, plex.love_threshold)
-
     services.lfm = None
     plex_relay_loves(services)
 
-    lbz_loves = services.lbz.all_loves()
-    assert len(lbz_loves) == settings.test_limit
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_loves(),
+        source_getter=plex.get_loved_tracks,
+        rating="love",
+    )
 
     # Un-love the tracks
+    lbz_loves = services.lbz.all_loves()
     for lbz_track in lbz_loves:
-        lbz_loves = services.lbz.reset(lbz_track)
+        services.lbz.reset(lbz_track)
     assert len(services.lbz.all_loves()) == 0
-
     # Sync again
     plex_relay_loves(services)
-
     # Check that the tracks were synced back to ListenBrainz
-    lbz_loves_after = services.lbz.all_loves()
-    assert len(lbz_loves_after) == settings.test_limit
+
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_loves(),
+        source_getter=plex.get_loved_tracks,
+        rating="love",
+    )
 
 
 def test_lbz_unhate(services, cleanup):
@@ -154,31 +160,36 @@ def test_lbz_unhate(services, cleanup):
     on ListenBrainz, it should be re-hated next sync
     """
     plex = services.plex
-
     # Make sure we're starting with no hated tracks
     assert len(plex.get_hated_tracks()) == 0
-
     # Search for arbitrary tracks and then hate them
     track_search = plex.music_library.search(libtype="track", limit=settings.test_limit)
+    # track_search = plex.music_library.search(
+    #     libtype="track", title="WELL I'LL BE A MONKEY"
+    # )
     for plex_track in track_search:
         plex.submit_rating(plex_track, plex.hate_threshold)
-
     services.lfm = None
     plex_relay_hates(services)
 
-    lbz_hates = services.lbz.all_hates()
-    assert len(lbz_hates) == settings.test_limit
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_hates(),
+        source_getter=plex.get_hated_tracks,
+        rating="hate",
+    )
 
     # Un-hate the tracks
-    for lbz_hate in lbz_hates:
+    for lbz_hate in services.lbz.all_hates():
         services.lbz.reset(lbz_hate)
-
-    lbz_hates = services.lbz.all_hates()
-    assert len(lbz_hates) == 0
-
+    assert len(services.lbz.all_hates()) == 0
     # Sync again
     plex_relay_hates(services)
-
     # Check that the track was synced back to ListenBrainz
-    lbz_hates_after = services.lbz.all_hates()
-    assert len(lbz_hates_after) == settings.test_limit
+
+    assert_relay_success(
+        expected=settings.test_limit,
+        actual=services.lbz.all_hates(),
+        source_getter=plex.get_hated_tracks,
+        rating="hate",
+    )

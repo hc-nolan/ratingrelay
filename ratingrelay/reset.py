773c6528c4a01038a7de7b6a745bdd3909a984e1
hnolan <hayden@hnolan.ca>
2025-12-04 19:38:33 -0500

    Add reset_track_log()

--- ratingrelay/reset.py
+++ ratingrelay/reset.py
@@ -2,9 +2,10 @@ import time
 import logging
 from urllib3.exceptions import ResponseError
 from .services import Services
-from .plex import Plex
+from .plex import Plex, PlexTrack
 from .listenbrainz import ListenBrainz
 from .lastfm import LastFM
+from .track import Track
 
 log = logging.getLogger("ratingrelay")
 
@@ -18,10 +19,11 @@ def reset_lbz(lbz: ListenBrainz):
     log.info(f"ListenBrainz: {len(hates)} tracks to unhate")
 
     tracks_to_reset = loves + hates
+    length = len(tracks_to_reset)
     i = 0
     for track in tracks_to_reset:
         i += 1
-        log.info(f"{i}/{len(tracks_to_reset)}")
+        reset_track_log(index=i, length=length, track=track)
         try:
             lbz.client.submit_user_feedback(0, track.mbid)
         except ResponseError as e:
@@ -37,10 +39,11 @@ def reset_lfm(lfm: LastFM):
     """Reset all loved tracks on LastFM"""
     loves = lfm.all_loves()
     log.info(f"Last.FM: {len(loves)} tracks to unlove")
+    length = len(loves)
     i = 0
     for track in loves:
         i += 1
-        log.info(f"{i}/{len(loves)}")
+        reset_track_log(index=i, length=length, track=track)
         lfm.reset(track)
 
 
@@ -56,11 +59,23 @@ def reset_plex(plex: Plex):
         hates = []
 
     tracks_to_reset = loves + hates
+    length = len(tracks_to_reset)
     i = 0
     for track in tracks_to_reset:
         i += 1
-        log.info(f"{i / len(tracks_to_reset)}: {track}")
+        reset_track_log(index=i, length=length, track=track)
         plex.submit_rating(track=track, rating=None)
+
+
+def reset_track_log(index: int, length: int, track: any):
+    match track:
+        case PlexTrack():
+            title = track.title
+            artist = track.artist().title
+        case Track():
+            title = track.title
+            artist = track.artist
+    log.info(f"({index} / {length}): Resetting {title} by {artist}")
 
 
 def reset(services: Services):
